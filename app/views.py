cfee276bb8bb159ea8d20ccdae811482ba3024a8
tungmk <tung03082004@gmail.com>
2025-04-18 19:54:27 +0700

    Thêm trang giỏ hàng và checkout

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def cart(request):
+    context= {}
+    return render(request, 'cart.html')
+
+def checkout(request):
+    context= {}
+    return render(request, 'checkout.html')
