204cb66713aee036ec7dbaeae8bf358195129bc2
cmdie311 <anticodon3624@gmail.com>
2025-04-21 23:34:07 +0700

    Xử lý phần giỏ hàng

--- app/views.py
+++ app/views.py
@@ -9,6 +9,13 @@ def home(request):
     return render(request, 'home.html', context)
 
 def cart(request):
+    if request.user.is_authenticated: 
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else: 
+        items = []
+    context = {'items':items, 'order':order}
     return render(request, 'cart.html', context)
 
 def checkout(request):
