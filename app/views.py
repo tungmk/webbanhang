2dbcab84c713df91cab5854560cbfdb8eb3ee848
cmdie311 <anticodon3624@gmail.com>
2025-04-22 21:19:03 +0700

    Cập nhật nút giỏ hàng

--- app/views.py
+++ app/views.py
@@ -5,8 +5,17 @@ import json
 # Create your views here.
 
 def home(request):
+    if request.user.is_authenticated: 
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else: 
+        items = []
+        order = {'get_cart_items':0,'get_cart_total':0} 
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
-    context= {'products': products} 
+    context= {'products': products, 'cartItems':cartItems} 
     return render(request, 'home.html', context)
 
 def cart(request):
@@ -14,10 +23,12 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else: 
         items = []
         order = {'get_cart_items':0,'get_cart_total':0} 
-    context = {'items':items, 'order':order}
+        cartItems = order['get_cart_items']
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'cart.html', context)
 
 def checkout(request):
@@ -25,10 +36,12 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer, complete = False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else: 
         items = []
         order = {'get_cart_items':0,'get_cart_total':0} 
-    context = {'items':items, 'order':order}
+        cartItems = order['get_cart_items']
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'checkout.html', context)
 
 def updateItem(request):
