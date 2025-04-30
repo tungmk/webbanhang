e3db79c2670805af98cafa9dc9c0ee15cdc5f9ec
bonnnn <161863346+yeuubonn2k4@users.noreply.github.com>
2025-04-30 23:48:33 +0700

    update Liên hệ + view chi tiết sản phẩm

--- app/static/app/js/cart.js
+++ app/static/app/js/cart.js
@@ -1,36 +1,42 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart');
 
-for (i=0; i<updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        var productID = this.dataset.product
-        var action = this.dataset.action
-        console.log('productID', productID, 'action', action)
-        console.log('user:', user)
-        if (user == "AnonymousUser"){
-            console.log('User is not logged in.')
+for (var i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productID = this.dataset.product;
+        var action = this.dataset.action;
+        var quantity = parseInt(this.closest('.product-container').querySelector('.quantity').value) || 1;  // Lấy số lượng từ input tương ứng
+
+        console.log('productID:', productID, 'action:', action, 'quantity:', quantity);
+        console.log('user:', user);
+
+        if (user == "AnonymousUser") {
+            console.log('User is not logged in.');
+        } else {
+            updateUserOrder(productID, action, quantity);
         }
-        else{
-            updateUserOrder(productID, action)
-        }
-    })
+    });
 }
 
-function updateUserOrder(productID, action){
-    console.log('User is logged in.')
-    var url = '/update_item/'
-    fetch(url,{
+function updateUserOrder(productID, action, quantity) {
+    console.log('User is logged in.');
+    var url = '/update_item/';
+
+    fetch(url, {
         method: 'POST',
-        headers:{
-            'Content-Type':'application/json',
-            'X-CSRFToken' : csrftoken ,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
         },
-        body: JSON.stringify({'productID':productID,'action':action})
+        body: JSON.stringify({
+            'productID': productID,
+            'action': action,
+            'quantity': quantity
+        })
     })
-    .then((response) =>{
-        return response.json()
-    })
-    .then((data) =>{
-        console.log('data',data)
-        location.reload()
-    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('data:', data);
+        // Cập nhật giỏ hàng trên giao diện người dùng mà không reload trang
+        document.getElementById('cart-total').innerText = data.cart_total;  // Giả sử dữ liệu trả về có thông tin giỏ hàng
+    });
 }
